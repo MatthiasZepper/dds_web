45e08b17fd8f6c2e5e66fb62f8bbf3483aaa7ce2
Ina Odén Österbo <ina.oden.osterbo@gmail.com>
2022-01-18 15:12:51 +0100

    bad time signature

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -89,13 +89,11 @@ def test_del_route_no_token(client):
 def test_del_route_invalid_token(client):
     """Use invalid signature"""
     client = tests.UserAuth(tests.USER_CREDENTIALS["delete_me_researcher"]).fake_web_login(client)
-    
+
     with pytest.raises(itsdangerous.exc.BadSignature):
         response = client.get(
             tests.DDSEndpoint.USER_CONFIRM_DELETE + "invalidtoken", content_type="application/json"
         )
-
-    assert response.status_code == http.HTTPStatus.BAD_REQUEST
 
 
 def test_del_route_expired_token(client):
@@ -103,11 +101,11 @@ def test_del_route_expired_token(client):
     token = "InJlc2VhcmNodXNlcjFAbWFpbHRyYXAuaW8i.YbIcrg.BmxUW6fKsnC3ujO5z1E_5CYiit4"
     client = tests.UserAuth(tests.USER_CREDENTIALS["delete_me_researcher"]).fake_web_login(client)
 
-    response = client.get(
-        tests.DDSEndpoint.USER_CONFIRM_DELETE + token, content_type="application/json"
-    )
+    with pytest.raises(itsdangerous.exc.BadTimeSignature);
+        response = client.get(
+            tests.DDSEndpoint.USER_CONFIRM_DELETE + token, content_type="application/json"
+        )
 
-    assert response.status_code == http.HTTPStatus.BAD_REQUEST
 
 
 def test_del_route_valid_token_wrong_user(client):
